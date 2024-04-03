50bd331ff9c888dba1147b8028475bad5045fcce
Hodak <82084934+HODAKdev@users.noreply.github.com>
2024-04-04 00:22:24 +0200

    Update ConstantBuffer.h

--- STRDX/src/ConstantBuffer.h
+++ STRDX/src/ConstantBuffer.h
@@ -62,16 +62,16 @@ public:
         return iProjection;
     }
     void Set(const float _Time,
-             const float _Fov,
              const float _Width,
              const float _Height,
-             const float _NearZ,
-             const float _FarZ,
-             const DirectX::XMFLOAT3& _Eye,
-             const DirectX::XMFLOAT3& _At,
-             const DirectX::XMFLOAT3& _Pos,
-             const DirectX::XMFLOAT3& _Rot,
-             const DirectX::XMFLOAT3& _Scale)
+             const float _Fov = 0.0f,
+             const float _NearZ = 0.0f,
+             const float _FarZ = 0.0f,
+             const DirectX::XMFLOAT3& _Eye = DirectX::XMFLOAT3(0, 0, 0),
+             const DirectX::XMFLOAT3& _At = DirectX::XMFLOAT3(0, 0, 0),
+             const DirectX::XMFLOAT3& _Pos = DirectX::XMFLOAT3(0, 0, 0),
+             const DirectX::XMFLOAT3& _Rot = DirectX::XMFLOAT3(0, 0, 0),
+             const DirectX::XMFLOAT3& _Scale = DirectX::XMFLOAT3(0, 0, 0))
     {
         iTime = _Time;
         iResolution = DirectX::XMFLOAT2(_Width, _Height);
@@ -81,8 +81,8 @@ public:
         DirectX::XMVECTOR Up = DirectX::XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
         iView = DirectX::XMMatrixLookAtLH(Eye, At, Up);
         iProjection = DirectX::XMMatrixPerspectiveFovLH(_Fov, _Width / _Height, _NearZ, _FarZ);
-        iWorld = DirectX::XMMatrixTranspose(iWorld);
-        iView = DirectX::XMMatrixTranspose(iView);
         iProjection = DirectX::XMMatrixTranspose(iProjection);
+        iView = DirectX::XMMatrixTranspose(iView);
+        iWorld = DirectX::XMMatrixTranspose(iWorld);
     }
 };
