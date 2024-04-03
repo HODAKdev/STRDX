5b153938c5b9c72efdee924142d797640bfa6eca
Hodak <82084934+HODAKdev@users.noreply.github.com>
2024-04-03 21:49:40 +0200

    Update Vertex.h

--- STRDX/src/Vertex.h
+++ STRDX/src/Vertex.h
@@ -3,10 +3,18 @@
 struct Vertex
 {
 public:
-	Vertex(float x, float y, float z) : x(x), y(y), z(z) {}
+	Vertex() : x(0), y(0), z(0), r(0), g(0), b(0), a(0) {}
+	Vertex(float x, float y, float z) : x(x), y(y), z(z), r(0), g(0), b(0), a(0) {}
+	Vertex(float x, float y, float z, float r, float g, float b, float a) : x(x), y(y), z(z), r(r), g(g), b(b), a(a) {}
 
-public:
+private:
 	float x;
 	float y;
 	float z;
+
+private:
+	float r;
+	float g;
+	float b;
+	float a;
 };
