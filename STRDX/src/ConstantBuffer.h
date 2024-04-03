91cf791904b8acf71e1adb3e0c74ff7918f04756
Hodak <82084934+HODAKdev@users.noreply.github.com>
2024-04-03 22:31:26 +0200

    Update ConstantBuffer.h

--- STRDX/src/ConstantBuffer.h
+++ STRDX/src/ConstantBuffer.h
@@ -11,7 +11,7 @@ public:
                        iView(DirectX::XMMatrixIdentity()),
                        iProjection(DirectX::XMMatrixIdentity()) {}
 
-public:
+public: // private for later
     float iTime;
     DirectX::XMFLOAT2 iResolution;
 
@@ -19,4 +19,46 @@ public:
     DirectX::XMMATRIX iWorld;
     DirectX::XMMATRIX iView;
     DirectX::XMMATRIX iProjection;
+
+public:
+    void SetTime(const float _Time)
+    {
+        iTime = _Time;
+    }
+    float GetTime()
+    {
+        return iTime;
+    }
+    void SetResolution(const DirectX::XMFLOAT2& _Resolution)
+    {
+        iResolution = _Resolution;
+    }
+    DirectX::XMFLOAT2 GetResolution()
+    {
+        return iResolution;
+    }
+    void SetWorld(const DirectX::XMMATRIX& _World)
+    {
+        iWorld = _World;
+    }
+    DirectX::XMMATRIX GetWorld()
+    {
+        return iWorld;
+    }
+    void SetView(const DirectX::XMMATRIX& _View)
+    {
+        iView = _View;
+    }
+    DirectX::XMMATRIX GetView()
+    {
+        return iView;
+    }
+    void SetProjection(const DirectX::XMMATRIX& _Projection)
+    {
+        iProjection = _Projection;
+    }
+    DirectX::XMMATRIX GetProjection()
+    {
+        return iProjection;
+    }
 };
